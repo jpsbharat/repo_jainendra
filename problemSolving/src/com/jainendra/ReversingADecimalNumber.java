52f4c1c8b300f10f9394567c285ae3a1e4f236df
jpsbharat <jpsbharat@users.noreply.github.com>
2019-06-17 14:08:29 +0530

    Update ReversingADecimalNumber.java

--- problemSolving/src/com/jainendra/ReversingADecimalNumber.java
+++ problemSolving/src/com/jainendra/ReversingADecimalNumber.java
@@ -3,41 +3,14 @@ package com.jainendra;
 import java.util.Stack;
 
 public class ReversingADecimalNumber {
-	public static void main(String[] args) {
-		test(100);
-		test(10);
-		test(101);
-		test(103);
-		test(1340);
-		test(340608090);
-		test(13406);
-		test(1340600);
-		test(987650);
-		test(134);
-		test(53267);
-		test(5689);
-		test(8956789);
-		test(5436);
-
-	}
-
-	public static void test(int number) {
-		int result = reverse(number);
-		System.out.println("Reverse of " + number + " is " + result);
-	}
 
 	public static int reverse(int number) {
 		int result = 0;
 		Stack<Integer> digits = new Stack<Integer>();
 
-		int mostSignificantMultiplier = 1;
-		int current = number;
-		while (mostSignificantMultiplier <= number) {
-			mostSignificantMultiplier = mostSignificantMultiplier * 10;
-		}
-		mostSignificantMultiplier = mostSignificantMultiplier / 10;
+		int mostSignificantMultiplier = getMostSignificantMultiplier(number);
 
-		current = number;
+		int current = number;
 		int currentDivisor = mostSignificantMultiplier;
 		while (currentDivisor > 0) {
 			int digit = current / currentDivisor;
@@ -53,5 +26,36 @@ public class ReversingADecimalNumber {
 			currentMultiplier = currentMultiplier / 10;
 		}
 		return result;
+	}
+
+	public static int getMostSignificantMultiplier(int number) {
+		int mostSignificantMultiplier = 1;
+		while (mostSignificantMultiplier <= number) {
+			mostSignificantMultiplier = mostSignificantMultiplier * 10;
+		}
+		mostSignificantMultiplier = mostSignificantMultiplier / 10;
+		return mostSignificantMultiplier;
+	}
+
+	public static void main(String[] args) {
+		test(100);
+		test(10);
+		test(101);
+		test(103);
+		test(1340);
+		test(340608090);
+		test(13406);
+		test(1340600);
+		test(987650);
+		test(134);
+		test(53267);
+		test(5689);
+		test(8956789);
+		test(5436);
+	}
+
+	public static void test(int number) {
+		int result = reverse(number);
+		System.out.println("Reverse of " + number + " is " + result);
 	}
 }
